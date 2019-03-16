b8c2a83a5a866f2ae4df62363669f55d2dbb1b54
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-16 15:47:38 -0300

    fwb: Add hide notch support [1/3]
    Include also:
    
    * Refresh nav gesture overlay when toggling notch
    
    Change-Id: I268fe8364bad801a48b33f5ac7da41863644aa14

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5880,6 +5880,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_UNIT_TYPE = "network_traffic_unit_type";
 
         /**
+         * Display cutout
+         * @hide
+         */
+        public static final String DISPLAY_CUTOUT_HIDDEN = "display_cutout_hidden";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6050,6 +6056,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
             PRIVATE_SETTINGS.add(INCALL_FEEDBACK_VIBRATE);
             PRIVATE_SETTINGS.add(AUTO_BRIGHTNESS_ONE_SHOT);
+            PRIVATE_SETTINGS.add(DISPLAY_CUTOUT_HIDDEN);
         }
         /**
          * These entries are considered common between the personal and the managed profile,
